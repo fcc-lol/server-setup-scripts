2f5e96f0200281df18ca7e9df42ee420a74d657e
Leo Mancini <mancini@leo.gd>
2025-06-20 11:34:21 -0400

    Removing unused dependencies

--- setup-new-react-app.sh
+++ setup-new-react-app.sh
@@ -229,27 +229,6 @@ if echo "{
     \"build\": \"react-scripts build\",
     \"test\": \"react-scripts test\",
     \"eject\": \"react-scripts eject\"
-  },
-  \"eslintConfig\": {
-    \"extends\": [
-      \"react-app\",
-      \"react-app/jest\"
-    ]
-  },
-  \"browserslist\": {
-    \"production\": [
-      \">0.2%\",
-      \"not dead\",
-      \"not op_mini all\"
-    ],
-    \"development\": [
-      \"last 1 chrome version\",
-      \"last 1 firefox version\",
-      \"last 1 safari version\"
-    ]
-  },
-  \"devDependencies\": {
-    \"@babel/plugin-proposal-private-property-in-object\": \"^7.21.11\"
   }
 }
 " | sudo tee $APPS_DIRECTORY/$APP_ID/package.json > /dev/null; then
